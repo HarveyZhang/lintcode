08c5ed7deba4113145dca4e430818a6f2ba3615b
Haowei Zhang <hwzh.me@gmail.com>
2018-01-09 22:46:01 -0800

    640. Edit Distance II version 2

--- 640_edit_distance_ii.py
+++ 640_edit_distance_ii.py
@@ -8,28 +8,18 @@ class Solution:
         # write your code here
         len_s = len(s)
         len_t = len(t)
-        distance = abs(len_s - len_t)
 
-        if distance > 1:
+        if len_s < len_t:
+            return self.isOneEditDistance(t, s)
+
+        if len_s - len_t > 1:
             return False
-        elif distance == 0:
-            one_different = False
-            for i in xrange(len_s):
-                if s[i] != t[i]:
-                    if one_different:
-                        return False
-                    else:
-                        one_different = True
-            if not one_different:
-                return False
-            else:
-                return True
-        else:
-            min_len = min(len_s, len_t)
-            for i in xrange(min_len):
-                if s[i] != t[i]:
-                    if len_s > len_t:
-                        return s[i + 1:] == t[i:]
-                    else:
-                        return s[i:] == t[i + 1:]
-            return True
+
+        for i in xrange(len_t):
+            if s[i] != t[i]:
+                if len_s == len_t:
+                    return s[i + 1:] == t[i + 1:]
+                else:
+                    return s[i + 1:] == t[i:]
+
+        return len_s != len_t
