d6a66772a53a41593c7a048db990e77c3ba7f88d
Haowei Zhang <hwzh.me@gmail.com>
2018-01-15 00:26:58 -0800

    76. Longest Increasing Subsequence (init)

---
+++ 76_longest_increasing_subsequence.py
@@ -0,0 +1,7 @@
+class Solution:
+    """
+    @param: nums: An integer array
+    @return: The length of LIS (longest increasing subsequence)
+    """
+    def longestIncreasingSubsequence(self, nums):
+        # write your code here
